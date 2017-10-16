188a1ec07c0bcf6584026b780ec680b08799b5df
4iki93 <4iki93@gmail.com>
2017-10-16 20:51:51 +0300

    mysql basa

--- src/main/java/ua/com/owu/entity/Main.java
+++ src/main/java/ua/com/owu/entity/Main.java
@@ -1,7 +1,25 @@
 package ua.com.owu.entity;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 public class Main {
     public static void main(String[] args) {
 
+
+        SessionFactory factory = new Configuration().configure().addAnnotatedClass(User.class).addAnnotatedClass(Product.class)
+                .buildSessionFactory();
+
+        Session session = factory.openSession();
+
+        session.beginTransaction();
+        session.save(new User ("Vasya","Peukin"));
+        session.getTransaction().commit();
+
+        session.close();
+        factory.close();
     }
 }
+
